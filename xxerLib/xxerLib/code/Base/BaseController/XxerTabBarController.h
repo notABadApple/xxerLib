7c75835dacb8bedcc3b70e31962b6f6bd7457ffb
Xxer <nachao@bendiyun.cn>
2019-09-17 14:27:50 +0800

    给项目添加pod和响应第三方库

---
+++ xxerLib/xxerLib/code/Base/BaseController/XxerTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  XxerTabBarController.h
+//  xxerLib
+//
+//  Created by Xxer on 2019/9/10.
+//  Copyright © 2019 Xxer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XxerTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ xxerLib/xxerLib/code/Base/BaseController/XxerTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  XxerTabBarController.m
+//  xxerLib
+//
+//  Created by Xxer on 2019/9/10.
+//  Copyright © 2019 Xxer. All rights reserved.
+//
+
+#import "XxerTabBarController.h"
+
+@interface XxerTabBarController ()
+
+@end
+
+@implementation XxerTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
+    return YES;
+}
+
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    UINavigationController *navi = (UINavigationController *)viewController;
+    DebugLog(@"点击tabbar didSelect的控制器 %@",navi.topViewController);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
